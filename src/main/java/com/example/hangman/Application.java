c463ba9766fab4da561e1e48d23714df27fc88e3
johnlyras98 <71226053+johnlyras98@users.noreply.github.com>
2022-01-07 10:36:38 +0200

    Add files via upload

--- src/main/java/com/example/hangman/Application.java
+++ src/main/java/com/example/hangman/Application.java
@@ -7,7 +7,6 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-
 public class Application extends javafx.application.Application {
 
     private static Stage stage ;
--- src/main/java/com/example/hangman/RoundStats.java
+++ src/main/java/com/example/hangman/RoundStats.java
@@ -1,27 +1,59 @@
 package com.example.hangman;
 
-import java.io.Serializable;
-
-public class RoundStats implements Serializable {
+/**
+ * This class is responsible for creating an object RoundStats with 4 values.
+ * <p>
+ * @author johnlyras98
+ */
+public class RoundStats{
     private int wins;
     private String selectedwords;
     private int tries;
     private int highscore;
+    /**
+     * Class constructor.
+     * @author johnlyras98
+     * @param wins
+     * @param selectedwords
+     * @param highscore
+     * @param tries
+     */
     public RoundStats(int wins, String selectedwords, int tries, int highscore){
         this.wins = wins;
         this.selectedwords = selectedwords;
         this.tries = tries;
         this.highscore = highscore;
     }
+
+    /**
+     * Getter for wins from the object RoundStats
+     * @author johnlyras98
+     * @return int wins
+     */
     public int getwins(){
         return wins;
     }
+    /**
+     * Getter for selectedwords from the object RoundStats
+     * @author johnlyras98
+     * @return selectedwords
+     */
     public String getselectedwords(){
         return selectedwords;
     }
+    /**
+     * Getter for tries from the object RoundStats
+     * @author johnlyras98
+     * @return tries
+     */
     public int gettries(){
         return tries;
     }
+    /**
+     * Getter for highscore from the object RoundStats
+     * @author johnlyras98
+     * @return highscore
+     */
     public int gethighscore(){
         return highscore;
     }
